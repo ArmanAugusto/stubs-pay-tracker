c2954c91ffebe7442d3808cd26e79fc8ab2d5771
Arman Augusto <arman.augusto@gmail.com>
2022-01-20 23:12:47 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *  main.c
+ *
+ *  driver file
+ *
+ */
+
+ int main(int argc, char *argv[]) {
+     printf("\n\n\t\t********  STUBS PAY TRACKER  ********\n\n");
+
+     return 0;
+ }
