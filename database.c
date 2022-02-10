0727435f7fd1388a6476620f95b9339bb0e4ed4e
arman <arman.augusto@gmail.com>
2022-02-09 22:13:50 -0800

    adjusted sign in credentials in database

--- database.c
+++ database.c
@@ -4,7 +4,7 @@
 #include "database.h"
 
 char connect_to_pg_db() {
-    PGconn *conn = PQconnectdb("user=arman password=TekkonKinkreet14! dbname=stubs_pay_tracker_db");
+    PGconn *conn = PQconnectdb("user=user password=password dbname=stubs_pay_tracker_db");
 
     if(PQstatus(conn) == CONNECTION_BAD) {
         fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(conn));
